9cbea73d5ae34d081621127335853e1edfaf4b24
Lucas <lucas.cordeiro@msn.com>
2020-03-24 12:50:18 -0300

    Initialize project

---
+++ GitKraken/GitKraken/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GitKraken
+//
+//  Created by Lucas Cordeiro on 24/03/20.
+//  Copyright © 2020 Sparks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+

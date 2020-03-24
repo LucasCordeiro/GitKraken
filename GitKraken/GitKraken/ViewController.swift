3e5a1eb95369c5c8cf47aa702ac1f6dfc75e62f2
Lucas <lucas.cordeiro@msn.com>
2020-03-24 13:23:32 -0300

    Altera;coes

---
+++ GitKraken/GitKraken/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitKraken
+//
+//  Created by Lucas Cordeiro on 24/03/20.
+//  Copyright © 2020 Sparks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        print("Alteração")
+        print("Alteração")
+        print("Alteração")
+        print("Alteração")
+        print("Alteração")
+        print("Alteração")
+    }
+}
+
